5110f088d1be69ea04653f292e8708fdd4806756
Ecaterina Scutelnic <ket1005210052@gmail.com>
2021-06-23 22:37:31 +0300

    final version of the code

---
+++ main.js
@@ -0,0 +1,14 @@
+let myAge = 25; //my age
+let earlyYears = 2; // early years
+earlyYears *=10.5;
+console.log(earlyYears); // return 21
+let  laterYears = myAge -2; //my age without 2
+console.log(laterYears); //return 23
+laterYears*=4;//dog years accounted for by my later years
+console.log(laterYears);
+console.log(earlyYears);
+const myAgeInDogYears = laterYears + earlyYears; //my dog years
+console.log(myAgeInDogYears);
+let myName1 ='Kate';
+let myName = myName1.toLowerCase();
+console.log('My name is ' +myName+'.I am '+myAge+ ' years old in human years which is '+ myAgeInDogYears+ ' years old in dog years.');
